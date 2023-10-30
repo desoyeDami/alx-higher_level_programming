02ba3f57849569ffb2c53e2a7ca4194032896697
desoyeDami <desoyedami@gmail.com>
2023-10-30 08:08:21 +0100

    Print number, float & string

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 98
+street_name = "Battery street"
+print(f"{number} {street_name}")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+str = "Holberton School"
+
+for n in range(0, 3):
+	print(str, end="")
+print(f"\n{str[0:9]}")
+
+
+
