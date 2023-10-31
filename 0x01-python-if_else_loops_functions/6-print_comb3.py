b3e28e2d1ab11b519fab09e186abf1c6c0f89d19
desoyeDami <desoyedami@gmail.com>
2023-11-01 00:03:04 +0100

    print last digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+two_digits = []
 for n in range(0, 100):
-    pass
+    if n < 99:
+        if n < 10:
+            n = f"0{n}"
+        else:
+            n = str(n)
+        for i in range(len(two_digits)):
+            if n[-1:0] == two_digits[i]:
+                two_digits.append("")
+        two_digits.append(n)
+print("{}".format(two_digits), end=", ")
+
+    # if n == 99:
+    #     print("{}".format(n))
