f650bc8014def0640e1e748a3e8b5b994a06a50c
desoyeDami <desoyedami@gmail.com>
2023-10-31 13:03:28 +0100

    Fix bug

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,9 @@
 def remove_char_at(str, n):
     i = 0
     res = ""
-    for i in range(len(str)):
+    str_len = len(str)
+    while i < str_len:
         if i != n:
             res += str[i]
+        i += 1
     return res
