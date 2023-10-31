b8c3d175b0d73ea2d4f7def616e7693bab43214c
desoyeDami <desoyedami@gmail.com>
2023-10-31 13:16:38 +0100

    Fix bug

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 n = 25
+m = 0
 while n != -1:
     if n % 2 != 0:
-        print(chr(n + 97), end="")
+        m = n + 97
     else:
-        print(chr(n + 65), end="")
+        m = n + 65
+    print(chr(m), end="")
     n -= 1
