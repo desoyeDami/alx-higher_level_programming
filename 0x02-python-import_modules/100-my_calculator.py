c3ffd4a91f199b1cbaf17428bc52feed082f63a8
desoyeDami <desoyedami@gmail.com>
2023-11-02 07:29:39 +0100

    My Calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import calculator_1
+import sys
+arg = sys.argv
+if len(arg) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+operator = arg[2]
+first_num = int(arg[1])
+second_num = int(arg[3])
+if operator == "+":
+    res = calculator_1.add(a=first_num, b=second_num)
+elif operator == "-":
+    res = calculator_1.sub(a=first_num, b=second_num)
+elif operator == "*":
+    res = calculator_1.mul(a=first_num, b=second_num)
+elif operator == "/":
+    res = calculator_1.div(a=first_num, b=second_num)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+print(f"{first_num} + {second_num} = {res}")
