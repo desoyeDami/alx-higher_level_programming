e0c97af3dc4aef912e0fdc4afd54badeda681574
desoyeDami <desoyedami@gmail.com>
2023-11-06 16:56:44 +0100

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if len(sentence) is None:
+    if len(sentence) == 0:
         return length, None
     else:
         first = sentence[0]
