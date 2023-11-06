dd50d6e1de1cdc3786bd967e566d07bca32cd67e
desoyeDami <desoyedami@gmail.com>
2023-11-06 17:06:28 +0100

    Max Number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     res = 0
     i = 0
-    while i < len(my_list):
+    while i < len(my_list) > 0:
         if res < my_list[i]:
             res = my_list[i]
         i += 1
