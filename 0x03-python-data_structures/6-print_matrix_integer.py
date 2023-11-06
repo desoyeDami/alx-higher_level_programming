2a93444f8818b53a8fdda20656b78dc02abb3d73
desoyeDami <desoyedami@gmail.com>
2023-11-06 16:23:36 +0100

    Print Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
+    for rw in matrix:
+        for item in rw:
+            print(item, end=" ")
+        print()
