a25ae8dbe08c55dfa0e862aadce67225cecd0074
desoyeDami <desoyedami@gmail.com>
2023-11-08 21:57:24 +0100

    Weight Average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is not None:
+    if my_list:
         numerator = sum([row[0] * row[1] for row in my_list])
         denominator = sum([row[1] for row in my_list])
         result = numerator / denominator
