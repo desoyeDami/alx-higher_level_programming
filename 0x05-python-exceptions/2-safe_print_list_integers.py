d5de03a4b48e4a248f57488958c531b188e7d3af
desoyeDami <desoyedami@gmail.com>
2023-12-22 02:47:25 +0100

    Safe Print List Integers Modified

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    item_count = 0
-    for num in range(0, x):
-        element = my_list[num]
-        if isinstance(element, int):
-            print("{:d}".format(element), end="")
-            item_count += 1
-    print()
-    if x > item_count:
+    try:
+        item_count = 0
+        for num in range(0, x):
+            element = my_list[num]
+            if isinstance(element, int):
+                print("{:d}".format(element), end="")
+                item_count += 1
+        print()
         return item_count
-    else:
-        return x
+    except SyntaxError:
+        pass
