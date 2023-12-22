9b7c62e8f13c3579f0e5aedb86be84aed0fc73a4
desoyeDami <desoyedami@gmail.com>
2023-12-22 01:33:47 +0100

    Align to Code Style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    #Get List length
-    count = 0
+    list_len = 0
     for _ in my_list:
-        count+=1
+        list_len += 1
     try:
-        for num in range(0,x):
+        for num in range(0, x):
             print(my_list[num], end="")
         print()
     except IndexError:
         print()
-        return count
+        return list_len
     else:
         return x
