f086b5fcf2efd7d95694e0a0284a2c4b560eb9e7
desoyeDami <desoyedami@gmail.com>
2024-01-21 01:30:35 +0100

    Fix a bug

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,12 +4,14 @@ Module: 1-square
 Defines a class Square with a private instance attribute.
 """
 
+
 class Square:
     """
     The Square class represents a geometric square.
 
     Attributes:
-        __size (int): Private instance attribute representing the size of the square.
+        __size (int): Private instance attribute
+        representing the size of the square.
 
     Methods:
         None
@@ -22,6 +24,7 @@ class Square:
             size (int): The size of the square.
 
         Note:
-            The size attribute is private and can only be accessed within the class.
+            The size attribute is private
+            and can only be accessed within the class.
         """
         self.__size = size
