4d09677bf25d18f463fb8618d2af1bfe79bc3e78
desoyedami <desoyedami@gmail.com>
2024-06-19 21:26:44 -0700

    FIx error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const input = args[2].trim();
-const toNumber = Number(input);
+const toNumber = Number(args[2]);
 
 if (isNaN(toNumber)) {
   console.log('Not a Number');
 } else {
-  console.log('My number: ', toNumber);
+  console.log('My number:', toNumber);
 }
