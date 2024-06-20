c133548b3a71d00d438ada60eb1804c8119cdb4b
desoyedami <desoyedami@gmail.com>
2024-06-19 21:36:37 -0700

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,8 @@ const args = Number(process.argv);
 if (isNaN(args[2]) || args.length === 3) {
   console.log(0);
 } else {
-  console.log('Yes');
+  const array = args.map(Number);
+  array.slice(2, args.length);
+  array.sort((a, b) => a - b);
+  console.log(array[array.length - 2]);
 }
