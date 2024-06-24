3b5f58f98dfe4a38cc44a90ae8625613eee7b993
desoyedami <desoyedami@gmail.com>
2024-06-23 17:18:11 -0700

    d

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+
+"""
+MySQL query script: Connects to MySQL DB and fetches rows from 'states' table.
+"""
 
 if __name__ == "__main__":
+    import sys      # OS interaction
+    import MySQLdb  # MySQL connector
 
-    MY_HOST = "localhost"
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
-    srch_name = sys.argv[4]
+    # Retrieve command-line arguments
+    MY_USER = sys.argv[1]   # MySQL username (provided as CLI argument)
+    MY_PASS = sys.argv[2]   # MySQL password (provided as CLI argument)
+    MY_DB = sys.argv[3]     # MySQL database name (provided as CLI argument)
+    srch_name = sys.argv[4]  # Name to search in 'states' table (CLI argument)
 
+    # MySQL database connection parameters
+    MY_HOST = "localhost"   # Assuming MySQL server is running locally
+
+    # Connect to MySQL database
     db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    # Create cursor to interact with database
     cur = db.cursor()
+
+    # Execute SELECT query to fetch rows from 'states' table where name
+    # matches srch_name
     cur.execute(
         "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (srch_name,))
 
+    # Fetch all rows returned by SELECT query
     rows = cur.fetchall()
+
+    # Iterate through fetched rows and print each row
     for row in rows:
         print(row)
-    # Close all cursors
+
+    # Close cursor to release database resources
     cur.close()
-    # Close all databases
+
+    # Close database connection
     db.close()
