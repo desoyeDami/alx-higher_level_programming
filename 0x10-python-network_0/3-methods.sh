4f0eb1ae8c15939d1161171888cad4fec304a86f
desoyedami <desoyedami@gmail.com>
2024-07-07 17:23:19 -0700

    Header

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# This script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sI -X OPTIONS "$1" | grep -i "Allow:" | cut -d ' ' -f2-
